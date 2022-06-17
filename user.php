60896cbf9116b36f1499fcccd40aadb4415f6c83
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-06-17 15:20:50 +0530

    Initial commit

---
+++ user.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <title>Crud Operation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container my-5">
+
+    <form method="post">
+        <div class="form-group">
+            <label>Name</label>
+            <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="off">
+        </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete="off">
+        </div>
+        <div class="form-group">
+            <label>Mobile</label>
+            <input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile" autocomplete="off">
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input type="text" class="form-control" placeholder="Enter your password" name="password" autocomplete="off">
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+</body>
+</html>
