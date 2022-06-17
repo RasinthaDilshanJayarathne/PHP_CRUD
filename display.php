249dc817cf168556f2f4ddcbbb8d9688684b83b1
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-06-17 16:22:14 +0530

    'Load All data in table'

---
+++ display.php
@@ -0,0 +1,78 @@
+<?php
+
+$con = new mysqli("127.0.0.1:3307","root","","crud");
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Crud Operation</title>
+</head>
+<body>
+
+<div class="container">
+    <button class="btn btn-primary my-5"><a href="user.php" class="text-light">Add user</a></button>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Sl no</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Mobile</th>
+            <th scope="col">Password</th>
+            <th scope="col">Operations</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+
+        $sql = "SELECT * FROM crud";
+        $result = mysqli_query($con,$sql);
+
+        if ($result){
+            while ($row = mysqli_fetch_assoc($result)){
+                $id = $row['id'];
+                $name = $row['name'];
+                $email = $row['email'];
+                $mobile = $row['mobile'];
+                $password = $row['password'];
+
+                echo '<tr>
+            <th scope="row">'.$id.'</th>
+            <td>'.$name.'</td>
+            <td>'.$email.'</td>
+            <td>'.$mobile.'</td>
+            <td>'.$password.'</td>
+
+        </tr>';
+            }
+        }
+
+        ?>
+       <!-- <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+        </tr>-->
+        </tbody>
+    </table>
+
+</div>
+
+</body>
+</html>
