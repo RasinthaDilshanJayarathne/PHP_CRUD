166963f500496d9e72dea0594a60cb5db0a3146d
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-06-19 15:01:17 +0530

    'Finished delete.php'

--- delete.php
+++ delete.php
@@ -1,1 +1,20 @@
 <?php
+
+include "connect.php";
+$con = new mysqli("127.0.0.1:3307", "root", "", "crud");
+
+
+if (isset($_GET['deleteid'])){
+    $id = $_GET['deleteid'];
+
+    $sql = "DELETE FROM crud WHERE id=$id";
+
+    $result = mysqli_query($con,$sql);
+    if ($result){
+        echo "Deleted successful";
+    }else{
+        die(mysqli_error($con));
+    }
+}
+
+?>
